4fe5a00a583657fde8359c836837895905101e2e
Jinwook Jeong <95017248+JinwookJeong030@users.noreply.github.com>
2022-09-23 23:45:02 +0900

    feat: 자잘한 사항 수정

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  HOST: "hs-eco.cngjojl6txsr.ap-northeast-2.rds.amazonaws.com",
+  HOST: "hs-eco-rds.cngjojl6txsr.ap-northeast-2.rds.amazonaws.com",
   USER: "root",
   PASSWORD: "onlyroot",
   DB: "eco",
